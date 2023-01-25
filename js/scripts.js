ffb3547685ab88c911bb3f15e10f0f84e346cd89
Michael Sol <sol.mike.j@gmail.com>
2023-01-25 15:10:14 -0800

    create a pass the turn method for Game and have each player pass after 4 successful rolls
    Co-authored-by: Zachary Cipolletta <ZackCipolletta@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ function Player(name) {
     this.name = name;
     this.turnTotal = 0;
     this.gameTotal = 0;
+    this.numberOfTimesRolled = 0;
 }
 
 Player.prototype.addToTurnTotal = function(dieRoll) {
@@ -19,9 +20,9 @@ Player.prototype.addTurnTotalToGameTotal = function() {
 }
 
 Player.prototype.checkValueOfCurrentDieRoll = function(dieRoll) {
+    this.numberOfTimesRolled++;
     if(dieRoll === 1) {
         this.zeroOutTurnTotal();
-        this.passTheTurn();
     } else {
         this.addToTurnTotal(dieRoll);
         this.checkWinState();
@@ -31,15 +32,13 @@ Player.prototype.checkValueOfCurrentDieRoll = function(dieRoll) {
 Player.prototype.checkWinState = function() {
     if(this.turnTotal + this.gameTotal >= 100) {
         console.log("Congratulations " + this.name + " you won!")
+        console.log(this.turnTotal + " " + this.gameTotal);
+        return true;
     }
+    return false;
 }
 
-Player.prototype.passTheTurn = function() {
-    console.log('passing to next player');
-}
-
-
-function dieRoll() {
+function rollDice() {
     let rollTheDie = Math.floor(Math.random()*6) + 1;
     return rollTheDie
 }
@@ -55,36 +54,38 @@ Game.prototype.addPlayersToGame = function(playerName) {
     this.playersArr.push(playerName);
 }
 
+Game.prototype.passTheTurn = function(currentGame) {  
+    let x = this.playersArr.shift();
+    this.playersArr.push(x);
+}
+
 //////////////////////////////////////////////////////////
 
-
-
-function gameloop() {
-    let ourGame = new Game();
-    while (ourGame.gameIsRunning) {
+function gameLoop(passedInGame) {
+    while (passedInGame.gameIsRunning) {
         //let currentPlayer = ourGame.playersArr[0];
-        ourGame.playersArr[0].dieRoll();
-        ourGame.playersArr[0].checkValueOfCurrentDieRoll(
-
+        let currentDiceRoll = rollDice();
+        passedInGame.playersArr[0].checkValueOfCurrentDieRoll(currentDiceRoll);
+        console.log(passedInGame.playersArr[0].name + " just rolled: " + currentDiceRoll);
+        if(passedInGame.playersArr[0].checkWinState() === true) {
+            passedInGame.gameIsRunning = false;
+        } else if (passedInGame.playersArr[0].numberOfTimesRolled >= 4 ) {
+            console.log('hello');
+            passedInGame.playersArr[0].numberOfTimesRolled = 0;
+            passedInGame.passTheTurn();
+        } else if(currentDiceRoll === 1){ // || player chooses to pass turn
+            passedInGame.playersArr[0].numberOfTimesRolled = 0;
+            passedInGame.passTheTurn();
+        } 
     }
 }
 
+// //////////////////////////////////////  Delte below 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let player1 = new Player('Bob');
+let player2 = new Player('Billy');
+let player3 = new Player('Thirsten');
+let newGame = new Game();
+newGame.addPlayersToGame(player2);
+newGame.addPlayersToGame(player1);
+newGame.addPlayersToGame(player3);
