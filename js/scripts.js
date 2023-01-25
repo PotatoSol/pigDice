43244cc7dbd39875398c5a7104f3d734ef3388b6
Michael Sol <sol.mike.j@gmail.com>
2023-01-25 10:35:40 -0800

    Add a method that will check the value of a die roll and call a different method if the roll is a 1 or not
    Co-authored-by: Zachary Cipolletta <ZackCipolletta@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,19 @@ Player.prototype.addToTurnTotal = function(dieRoll) {
     this.turnTotal += dieRoll;    
 };
 
-Player.prototype.playerRolledOne = function() {
+Player.prototype.zeroOutTurnTotal = function() {
     this.turnTotal = 0;
+}
+
+Player.prototype.addTurnTotalToGameTotal = function() {
+    this.gameTotal += this.turnTotal;
+    this.zeroOutTurnTotal();
+}
+
+Player.prototype.checkValueOfCurrentDieRoll = function(dieRoll) {
+    if(dieRoll === 1) {
+        this.zeroOutTurnTotal();
+    } else {
+        this.addToTurnTotal(dieRoll);
+    }
 }
